a7af4647dcbdeae2256c8fee88ae8fbffefe160d
Yaqubboy <yaqubboyyaqubboyev45@gmail.com>
2022-05-08 17:35:24 +0500

    installing packages and setting up my firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBNUYFjGBs2qnvcelz0-92lhf1SV86bryk',
+  authDomain: 'facebook-clone-838d3.firebaseapp.com',
+  projectId: 'facebook-clone-838d3',
+  storageBucket: 'facebook-clone-838d3.appspot.com',
+  messagingSenderId: '7412990492',
+  appId: '1:7412990492:web:685ce9a67232a2af3d95d9',
+}
