4a3d2208157ba79d08b6b8a323c9a3797be2a8c2
Yaqubboy <yaqubboyyaqubboyev45@gmail.com>
2022-05-09 09:09:30 +0500

    messagesender is complete

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './MessageSender.css'
 import Avatar from '@mui/material/Avatar'
+import VideocamIcon from '@mui/icons-material/Videocam'
+import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary'
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon'
 
 function MessageSender() {
+  const [input, setInput] = useState('')
+  const [imageUrl, setImageUrl] = useState('')
   const handleSubmit = (e) => {
     e.preventDefault()
+    // db stuff
+
+    setInput('')
+    setImageUrl('')
   }
   return (
     <div className='messageSender'>
@@ -12,17 +21,37 @@ function MessageSender() {
         <Avatar />
         <form>
           <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
             type='text'
             placeholder="What's on your mind?"
             className='messageSender__input'
           />
-          <input type='text' placeholder='image URL (Optional)' />
+          <input
+            type='text'
+            placeholder='image URL (Optional)'
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+          />
           <button onClick={handleSubmit} type='submit'>
             Hidden Sumit
           </button>
         </form>
       </div>
-      <div className='messageSender__bottom'></div>
+      <div className='messageSender__bottom'>
+        <div className='messageSender__option'>
+          <VideocamIcon style={{ color: 'red' }} />
+          <h3>Live Video</h3>
+        </div>
+        <div className='messageSender__option'>
+          <PhotoLibraryIcon style={{ color: 'green' }} />
+          <h3>Photo/Video</h3>
+        </div>
+        <div className='messageSender__option'>
+          <InsertEmoticonIcon style={{ color: 'orange' }} />
+          <h3>Feeling/Activity</h3>
+        </div>
+      </div>
     </div>
   )
 }
